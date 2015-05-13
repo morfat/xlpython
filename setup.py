f28aa47f9545d48a04777ca1b95fe7fc02f180c1
morfat <morfatmosoti@gmail.com>
2015-05-13 14:01:38 +0300

    final version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'xlpython',
-  version = '0.1',
+  version = '0.2.1',
   description = 'A small and simple class for processing excel files in python',
   author = 'Morfat Mosoti Ogega',
   author_email = 'morfatmosoti@gmail.com',
   url = 'https://github.com/morfat/xlpython', # use the URL to the github repo
   keywords = ['processing', 'python', 'excel'], # arbitrary keywords
   install_requires=['xlrd'],
-  py_modules=['xlpython'],
-  packages='xlpython',
+  py_modules=['excel'],
+  packages=['xlpython'],
 )
 
